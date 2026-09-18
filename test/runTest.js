3a2b878b744a2e96ed46bdbb788a75fe2f6b5ab5
Scott Motte <mot@mot.la>
2026-09-18 15:32:54 -0700

    audit fix

--- test/runTest.js
+++ test/runTest.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const fs = require('fs/promises')
+const os = require('os')
 
 const { runTests } = require('@vscode/test-electron')
 
 async function main () {
+  const workspacePath = await fs.mkdtemp(path.join(os.tmpdir(), 'dotenv-vscode-test-'))
   try {
+    await fs.writeFile(path.join(workspacePath, '.env'), 'HELLO=World\n')
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../')
@@ -13,10 +17,16 @@ async function main () {
     const extensionTestsPath = path.resolve(__dirname, './suite/index')
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath })
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [workspacePath, '--disable-extensions', '--skip-welcome', '--skip-release-notes']
+    })
   } catch (err) {
-    console.error('Failed to run tests')
-    process.exit(1)
+    console.error('Failed to run tests', err)
+    process.exitCode = 1
+  } finally {
+    await fs.rm(workspacePath, { recursive: true, force: true })
   }
 }
 
